77db2f92d537d824267f2f10b4da03510a49eb49
Eric Culp <eculperic@gmail.com>
2023-12-03 21:29:50 -0700

    Day 2

---
+++ 02/p2.nix
@@ -0,0 +1,27 @@
+#! /usr/bin/env -S nix eval --file
+let
+  lib = (import <nixpkgs> {}).lib;
+  txt = builtins.readFile ./input.txt;
+  lines = lib.filter (line: line != "") (lib.splitString "\n" txt);
+  match = re: v: let m = builtins.match re v; in if m == null then throw "${re} not matched: '${v}'" else m;
+  max = a: b: if a > b then a else b;
+  games = builtins.map (line:
+    let
+      m = match "Game ([0-9]+): (.*)" line;
+      id = lib.toIntBase10 (builtins.elemAt m 0);
+      rounds = lib.splitString "; " (builtins.elemAt m 1);
+      maxs = builtins.foldl' (acc: balls:
+        builtins.foldl' (acc: ball:
+          let
+            m = match "([0-9]+) (red|green|blue)" ball;
+            n = lib.toIntBase10 (builtins.elemAt m 0);
+            color = builtins.elemAt m 1;
+          in
+            (acc // {${color}=(max n acc.${color});})
+        ) acc (lib.splitString ", " balls)
+      ) {red=0; green=0; blue=0;} rounds;
+    in
+      {id=id; power= maxs.red * maxs.green * maxs.blue;}
+  ) lines;
+in
+  lib.foldl' (acc: g: acc+g.power) 0 games
